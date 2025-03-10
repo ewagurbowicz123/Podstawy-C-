d6fc2f5a75d7580e79f1b93efb13752597dd5620
Ewa Gurbowicz <e.gurbowicz.576@studms.ug.edu.pl>
2025-03-10 16:18:19 +0100

    pozdrawiam

---
+++ zadanie 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący rok i wypisujący na ekranie informację czy jest to rok przestęp czy też nie
+
+            int rok;
+            Console.WriteLine("podaj rok");
+            rok = Convert.ToInt32(Console.ReadLine());
+
+            if((rok % 4) & (rok! % 100) && (rok % 400 == 0))
+
+            { Console.WriteLine("rok jest przestępny"); }
+
+            else { Console.WriteLine("rok nie jest przestępny"); }
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
---
+++ zadanie 6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //. Napisać program wczytujący numer miesiąca (od 1 do 12) i wypisujący na ekranie nazwę tegemiesiąca.Użyj instrukcji wyboru switch.
+
+
+            Console.WriteLine("podaj nume)");
+            int miesiac;
+            miesiac = Convert.ToInt32(Console.ReadLine());
+            switch (miesiac)
+            {
+                case 1: Console.WriteLine("styczeń"); break;
+                case 2: Console.WriteLine("luty"); break;
+                case 3: Console.WriteLine("marzec"); break;
+                case 4: Console.WriteLine("kwiecień"); break;
+                case 5: Console.WriteLine("maj"); break;
+                case 6: Console.WriteLine("czerwiec"); break;
+                case 7: Console.WriteLine("lipiec"); break;
+                case 8: Console.WriteLine("sierpień"); break;
+                case 9: Console.WriteLine("wrzesień"); break;
+                case 10: Console.WriteLine("październik"); break;
+                case 11: Console.WriteLine("listopad"); break;
+                case 12: Console.WriteLine("grudzień"); break;
+                default: Console.WriteLine("nie ma takiego miesiąca"); break;
+            }
+                    Console.ReadKey(true);
+
+            
+        }
+    }
+}
