6a9cc849fa885e2057011b78e866b217899c6246
EwaGurbowicz <ewagurbo@gmail.com>
2025-03-27 00:48:20 +0100

    do dokończenia :)

---
+++ 5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący od użytkownika liczbę rzeczywistą x. Wczytuj tak długo wartość
+            //zmiennej x, aż przyjmie ona wartość dodatnią. Wczytaną dodatnią wartość liczby x wypisz na
+            //ekranie.Użyj pętli do -while.
+            int i;
+
+            do
+            {
+                Console.WriteLine("podaj liczbę:");
+                i = Convert.ToInt32(Console.ReadLine());
+                
+            }
+            while (i <= 0);
+            Console.WriteLine("liczba {0} jest dodatnia",i); 
+            Console.ReadKey(true);
+
+        }
+    }
+}
---
+++ 6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Napisać program, który będzie umożliwiał użytkownikowi obliczenia pól figur płaskich. 
+            //Jeżeli użytkownik wybierze 1, to powinien mieć możliwość obliczenia pola prostokąta,   
+            //dla 2 – pola kwadratu, a dla 3 – pola trójkąta. Program powinien pytać użytkownika, które
+            //pole chce obliczyć aż do momentu, kiedy użytkownik wpisze 0(zero).
+
+            Console.WriteLine(" ");
+        }
+    }
+}
