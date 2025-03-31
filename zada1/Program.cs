b16b884205616caa1d9acaaaeb9d57758f924343
EwaGurbowicz <ewagurbo@gmail.com>
2025-03-31 21:27:17 +0200

    hihi

---
+++ zada1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zada1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący współrzędne punktu P=(x,y) i wypisujący na ekranie informację, 
+            //do której ćwiartki ten punkt należy, czy też informację, że leży on na osi OX i/ lub osi OY. 
+
+            int x, y;
+            Console.WriteLine("podaj x:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("podaj y");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            if (x > 0 && y > 0) { Console.WriteLine("Punkt należy do I ćwiartki."); }
+            else if (x < 0 && y > 0) { Console.WriteLine("Punkt należy do II ćwiartki."); }
+            else if (x < 0 && y < 0) { Console.WriteLine("Punkt należy do  ćwiartki."); }
+            else Console.WriteLine("Punkt należy do IV ćwiartki.");
+
+            if (y == 0 && x == 0) { Console.WriteLine("punkt leży w centrum"); }
+            else if (x == 0) { Console.WriteLine("dodatkowo punkt leży na osi 0Y "); }
+            else if  (y == 0) { Console.WriteLine("dodatkowo punkt leży na osi 0X"); } 
+
+                Console.ReadKey(true);
+
+        }
+    }
+}
---
+++ zada2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zada2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Napisać program wczytujący współrzędne dwóch punktów A=(x1,y1) i B=(x2,y2) oraz wypisujący na ekranie wzór funkcji liniowej przechodzącej przez te dwa punkty.
+
+            double x1, x2, y1, y2;
+            Console.WriteLine("podaj wartość x1:");
+            x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("podaj wartość y1:");
+            y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("podaj wartość x2:");
+            x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("podaj wartość y2:");
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+
+            double a = (y2 - y1) / (x2 - x1);
+            double b = y1 - a * x1;
+
+            if (x1 == x2) {Console.WriteLine("Funkcja liniowa nie istnieje – prosta jest pionowa: x = " + x1); }
+            else if (a == 0 && a == 1) { Console.WriteLine($"Wzór funkcji liniowej: y = x + {b}"); }
+            else  Console.WriteLine($"Wzór funkcji liniowej: y = {a}x + {b}");
+            
+            Console.WriteLine("");
+            
+        }
+    }
+}
