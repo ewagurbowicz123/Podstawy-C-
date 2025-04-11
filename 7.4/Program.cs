3ffbad1a6f30de4c43f26fbbc397e158f2765d11
Jan Rafałko <j.rafalko.015@studms.ug.edu.pl>
2025-04-11 12:08:03 +0200

    zobaczymy czy wypchnie/koniec zajec

---
+++ 7.4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._4
+{
+    internal class Program
+    {
+        static int ile_cyfr(int liczba)
+        {
+            int i = 0;
+            while (liczba /10> 0)
+            {
+                liczba /= 10;
+                i++;
+
+            }
+            return i+1;
+        }
+        static void Main(string[] args)
+        {
+            //Napisz funkcję o nazwie ile_cyfr, która wyznacza liczbę cyfr dla zadanej liczby naturalnej jako argument tej funkcji. Napisz program wykorzystujący funkcję ile_cyfr
+            Console.WriteLine("liczba 0 ma {0} cyfr", ile_cyfr(0));
+            Console.WriteLine("Liczba 1234 ma {0} cyfr", ile_cyfr(1234));
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
---
+++ 7.55/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._55
+{
+    internal class Program
+    {
+        static int czy_pierwsza(int liczba)
+        {
+            int i;
+            for (i=2;i< liczba; i++)
+            {
+                if (liczba % i == 0)
+                {
+                    return 0;
+                }
+                return 1;
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            //Napisz funkcję, która sprawdza czy jej argument jest liczbą pierwszą. Jako wartość funkcja
+            //powinna zwracać 1 dla liczb pierwszych i 0 dla pozostałych liczb.Przetestuj działanie
+            //utworzonej funkcji
+
+            
+            Console.WriteLine("Liczba  jest pierwsza", czy_pierwsza(12));
+            Console.ReadKey(true);
+
+        }
+    }
+}
