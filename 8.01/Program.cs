6f23515e4b552f11a07fe225e3a0b78f1a152a63
EwaGurbowicz <ewagurbo@gmail.com>
2025-05-03 19:02:42 +0200

    koniec

---
+++ 8.01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._01
+{
+    internal class Program
+    {
+        static long silnia(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * silnia(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę do obliczenia silni:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0)
+            {
+                Console.WriteLine("bląd :)");
+            }
+            else
+            {
+                
+                Console.WriteLine($"{n}!={silnia(n)}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ 8.02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._02
+{
+    internal class Program
+    {
+        static long fib(long n)
+        {
+            if (n == 0) return 0;
+            else if (n==1) return 1;
+            else return fib(n - 1) + fib(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("podaj n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0) Console.Write("błąd");
+            else Console.WriteLine("Fib({0})={1}", n, fib(n));
+            Console.ReadKey(true);
+        }
+    }
+}
