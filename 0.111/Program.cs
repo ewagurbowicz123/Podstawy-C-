6aff396ce046f4c2af21f992474d0c7bf7934d09
EwaGurbowicz <ewagurbo@gmail.com>
2025-05-20 20:21:17 +0200

    koniec

---
+++ 0.111/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._111
+{
+    internal class Program
+    {
+        static double pole(double p, double q)
+        {
+            return (p * q) / 2;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("pole deltoidu: {0}", pole(4, 5));
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
---
+++ 0.112/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._112
+{
+    internal class Program
+    {
+        //Napisać funkcję o nazwie NWD zwracającą wartość największego wspólnego dzielnika dla jej 
+        //dwóch argumentów będących liczbami naturalnymi.
+        static int NWD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NWD(24, 36): " + NWD(24, 36));
+            Console.ReadKey(true);
+        }
+    }
+}
