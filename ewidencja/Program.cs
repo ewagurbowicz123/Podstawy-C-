8b0f68199c5264370d21bb0c3cc982f175ac0c80
EwaGurbowicz <ewagurbo@gmail.com>
2025-05-20 19:56:42 +0200

    koniec

---
+++ ewidencja/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ewidencja
+{
+    struct poprawne
+    {
+        public int a, b, c, d;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("podaj swoje imie i nazwisko:");
+
+
+        }
+    }
+}
