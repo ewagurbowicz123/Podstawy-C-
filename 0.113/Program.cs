7ddfe7e719da7f741b92cb929ddb57c525d86c55
EwaGurbowicz <ewagurbo@gmail.com>
2025-05-24 23:50:45 +0200

    koniec

---
+++ 0.113/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._113
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[1000];
+            Console.Write("poodaj n");
+            int i, n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 0; i<n;i++)
+            {
+                Console.WriteLine("podaj tab[{0}]", i);
+                tab[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            int min, max;
+            min = tab[0];
+            max = tab[0];
+
+            for (i = 0; i < n; i++) 
+            {
+                if (tab[i] < min)
+                    min = tab[i];
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                if (tab[i] > max)
+                    max = tab[i];
+
+            }
+
+
+                Console.ReadKey(true);
+        }
+    }
+}
---
+++ 0.114/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._114
+{
+    internal class Program
+    {
+        static int pierwsza(int n)
+        {
+            if (n % n == 0 && n % 1 == 0) return 1;
+            else return 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            // Napisz funkcję o nazwie czy_kw, która stwierdza, czy zadana jako parametr liczba całkowita
+            // jest kwadratem pewnej liczby całkowitej.Liczby będące kwadratami liczb całkowitych to 1, 4,
+            // 9, 16, 25, itd.Wartością funkcji ma być jeden, jeśli liczba spełnia warunek lub zero w
+            // przeciwnym przypadku.Napisz program testujący utworzoną funkcję czy_kw.
+
+
+            Console.WriteLine("{0}", pierwsza(6));
+            
+            
+            
+
+            
+            Console.ReadKey(true);
+
+        }
+    }
+}
