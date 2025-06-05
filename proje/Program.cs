2a780153055c7a028390d5525cdd9e2b476b90b7
EwaGurbowicz <ewagurbo@gmail.com>
2025-06-05 14:10:13 +0200

    koniec

---
+++ proje/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projekt znajduje się w osobnym repozytorium, po prostu nie widziałam jak usunąć " +
+                "tą gałąź");
+            Console.ReadKey();
+        }
+    }
+}
